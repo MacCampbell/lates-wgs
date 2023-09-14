6bb2ded29ed96982841879fb563dd3461e7bc00b
maccampbell <campbellmatthewa@gmail.com>
2023-09-14 13:56:53 -0800

    Depth

---
+++ 201.1-depth.sh
@@ -0,0 +1,6 @@
+samtools depth -a -r NC_066849.1 data/split/SRR3165620.sort.flt.bam > outputs/201/SRR3165620.cov
+samtools depth -a -r NC_066849.1 data/split/SRR3165595.sort.flt.bam > outputs/201/SRR3165595.cov
+samtools depth -a -r NC_066849.1 data/split/SRR3183260.sort.flt.bam > outputs/201/SRR3183260.cov
+samtools depth -a -r NC_066849.1 data/split/SRR3165615.sort.flt.bam > outputs/201/SRR3165615.cov
+samtools depth -a -r NC_066849.1 data/split/SRR3165609.sort.flt.bam > outputs/201/SRR3165609.cov
+samtools depth -a -r NC_066849.1 data/split/SRR3183265.sort.flt.bam > outputs/201/SRR3183265.cov
