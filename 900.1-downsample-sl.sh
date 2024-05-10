e7cec3acecbd8d73ecdda6477ed514c269fc55ef
Mac Campbell <mac@macs-mbp.building.sydney.edu.au>
2024-05-10 10:00:53 +1000

    Setting up to downsample

---
+++ 900.1-downsample-sl.sh
@@ -0,0 +1,10 @@
+samtools view -bs 0.308416238555555 data/lakdiva-trim/Mannar1.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mannar1.reduced.bam
+samtools view -bs 0.232548095929412 data/lakdiva-trim/Mannar2.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mannar2.reduced.bam
+samtools view -bs 0.307674726279377 data/lakdiva-trim/Mannar3.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mannar3.reduced.bam
+samtools view -bs 0.317336549010363 data/lakdiva-trim/Mannar4.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mannar4.reduced.bam
+samtools view -bs 0.286968749103223 data/lakdiva-trim/Mannar5.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mannar5.reduced.bam
+samtools view -bs 0.292074353785492 data/lakdiva-trim/Trinco1.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Trinco1.reduced.bam
+samtools view -bs 0.236135474294967 data/lakdiva-trim/Trinco2.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Trinco2.reduced.bam
+samtools view -bs 0.26307086377439 data/lakdiva-trim/Trinco3.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Trinco3.reduced.bam
+samtools view -bs 0.264709330247579 data/lakdiva-trim/Jaffna1.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Jaffna1.reduced.bam
+samtools view -bs 0.221555445832859 data/lakdiva-trim/Jaffna2.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Jaffna2.reduced.bam
