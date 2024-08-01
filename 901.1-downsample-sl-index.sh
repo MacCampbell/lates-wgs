ae16f09f07dd29634528cd7871e78cc5728e7f98
Mac Campbell <mac@macs-mbp.building.sydney.edu.au>
2024-08-02 09:53:16 +1000

    Editing downsample commands

---
+++ 901.1-downsample-sl-index.sh
@@ -0,0 +1,15 @@
+samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream1.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream2.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream3.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream4.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream5.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick11.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick15.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick16.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick17.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick1.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick20.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick2.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick3.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick5.reduced.bam;
+samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick8.reduced.bam;
--- 901.1-downsample-sl.sh
+++ 901.1-downsample-sl.sh
@@ -1,10 +1,10 @@
-samtools view -bs 0.345084274509611 data/hatch-trim/Mainstream1.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream1.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream1.reduced.bam
-samtools view -bs 0.314359492533962 data/hatch-trim/Mainstream2.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream2.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream2.reduced.bam
-samtools view -bs 0.281051131230803 data/hatch-trim/Mainstream3.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream3.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream3.reduced.bam
-samtools view -bs 0.362775126064356 data/hatch-trim/Mainstream4.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream4.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream4.reduced.bam
-samtools view -bs 0.275900124155056 data/hatch-trim/Mainstream5.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream5.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Mainstream5.reduced.bam
-samtools view -bs 0.30213305939936 data/hatch-trim/OceanPick15.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick15.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick15.reduced.bam
-samtools view -bs 0.284551688814273 data/hatch-trim/OceanPick16.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick16.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick16.reduced.bam
-samtools view -bs 0.321902720997714 data/hatch-trim/OceanPick17.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick17.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick17.reduced.bam
-samtools view -bs 0.226592214938901 data/hatch-trim/OceanPick3.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick3.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick3.reduced.bam
-samtools view -bs 0.300864340268973 data/hatch-trim/OceanPick5.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick5.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/OceanPick5.reduced.bam
+samtools view -bs 0.345084274509611 data/hatch-trim/Mainstream1.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream1.reduced.bam
+samtools view -bs 0.314359492533962 data/hatch-trim/Mainstream2.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream2.reduced.bam
+samtools view -bs 0.281051131230803 data/hatch-trim/Mainstream3.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream3.reduced.bam
+samtools view -bs 0.362775126064356 data/hatch-trim/Mainstream4.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream4.reduced.bam
+samtools view -bs 0.275900124155056 data/hatch-trim/Mainstream5.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Mainstream5.reduced.bam
+samtools view -bs 0.30213305939936 data/hatch-trim/OceanPick15.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick15.reduced.bam
+samtools view -bs 0.284551688814273 data/hatch-trim/OceanPick16.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick16.reduced.bam
+samtools view -bs 0.321902720997714 data/hatch-trim/OceanPick17.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick17.reduced.bam
+samtools view -bs 0.226592214938901 data/hatch-trim/OceanPick3.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick3.reduced.bam
+samtools view -bs 0.300864340268973 data/hatch-trim/OceanPick5.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/OceanPick5.reduced.bam
