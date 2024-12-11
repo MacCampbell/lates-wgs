6270cb9651ddd1576480d16991960dffc0a81974
Mac Campbell <mac@macs-mbp.staff.wireless.sydney.edu.au>
2024-12-11 11:04:43 +1100

    Downloading raw sequences for assemblies again

---
+++ 1203.1-split.sh
@@ -0,0 +1,11 @@
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3165613.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3165614.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3165615.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3165616.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3165617.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3183267.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3183268.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3183269.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3183270.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3183271.sralite.1
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip SRR3165612.sralite.1
