f08eb22e0fc0dbae7d26a8d4ec8672a78b88ecaa
Mac Campbell <mac@macs-mbp.building.sydney.edu.au>
2024-12-13 13:06:35 +1100

    Getting wgs in there!!

--- 1203.1-split.sh
+++ 1203.1-split.sh
@@ -1,3 +1,4 @@
+fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip data/raw/SRR3165613.sralite.1
 fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip data/raw/SRR3165614.sralite.1
 fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip data/raw/SRR3165615.sralite.1
 fastq-dump --outdir data/denovo --skip-technical --read-filter pass --dumpbase --split-3 --gzip --clip data/raw/SRR3165616.sralite.1
