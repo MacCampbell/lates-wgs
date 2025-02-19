b25b81fa6efcd8639e498e923472bd8a1d1ea234
Mac Campbell <campbellmatthewa@gmail.com>
2025-02-19 15:31:26 +1100

    Working up Taiwan, looking at Broome

---
+++ 1300.1-downsample.sh
@@ -0,0 +1,11 @@
+samtools view -bs 0.191806045726561 data/broome/Broome01.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome01.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome01.reduced.bam ;
+samtools view -bs 0.1867722552483 data/broome/Broome02.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome02.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome02.reduced.bam ;
+samtools view -bs 0.214516648024148 data/broome/Broome03.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome03.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome03.reduced.bam ;
+samtools view -bs 0.203059821423391 data/broome/Broome04.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome04.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome04.reduced.bam ;
+samtools view -bs 0.203444016566156 data/broome/Broome05.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome05.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome05.reduced.bam ;
+samtools view -bs 0.200747353719705 data/broome/Broome06.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome06.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome06.reduced.bam ;
+samtools view -bs 0.206608521125721 data/broome/Broome07.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome07.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome07.reduced.bam ;
+samtools view -bs 0.169201465768126 data/broome/Broome08.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome08.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome08.reduced.bam ;
+samtools view -bs 0.186341190720209 data/broome/Broome09.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome09.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome09.reduced.bam ;
+samtools view -bs 0.21712495541184 data/broome/Broome10.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Broome10.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Broome10.reduced.bam ;
+samtools view -bs 0.179093174503272 data/broome/Fitzroy01.sort.flt.bam > /home/maccamp/lates-wgs/data/downsample/Fitzroy01.reduced.bam ; samtools index /home/maccamp/lates-wgs/data/downsample/Fitzroy01.reduced.bam ;
